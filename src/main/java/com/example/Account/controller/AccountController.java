95c325613f2e379578cf1f68248fc00aedbf32a7
jooyeon <jooyeon0907@naver.com>
2024-12-13 01:17:26 +0900

    Embedded Redis 실행하여 락 획득하기
    Part 07. 스프링 프레임 워크
    Chapter 07. 사전준비
    07. Embedded Redis
    
    -----
    기존 jdk21 버전이 java.util.concurrent.TimeUnit 를 import 하지 못하는 이슈로
    jdk17 로 다운그레이드

--- src/main/java/com/example/Account/controller/AccountController.java
+++ src/main/java/com/example/Account/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.example.Account.controller;
 
 import com.example.Account.domain.Account;
 import com.example.Account.service.AccountService;
+import com.example.Account.service.RedisTestService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AccountController {
 	private final AccountService accountService;
+	private final RedisTestService redisTestService;
+
+	@GetMapping("/get-lock")
+	public String getLock() {
+		return redisTestService.getLock();
+	}
 
 	@GetMapping("/create-account")
 	public String createAccount(){
