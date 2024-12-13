6d0d59cac691bdf3f6c3ed36c0ceceeac25746ea
jooyeon <jooyeon0907@naver.com>
2024-12-13 19:46:03 +0900

    service 테스트 추가
    Part 07. 스프링 프레임 워크
    Chapter 07. 사전준비
    08. 테스트

--- src/test/java/com/example/Account/service/AccountServiceTest.java
+++ src/test/java/com/example/Account/service/AccountServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,8 +17,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 
 @ExtendWith(MockitoExtension.class) // Mockito 테스트를 위한 어노테이션
@@ -42,13 +46,41 @@ class AccountServiceTest {
 							)
 				);
 
+		ArgumentCaptor<Long> captor = ArgumentCaptor.forClass(Long.class); // Long 타입 박스 만들기
+
+
+
 		// when
 		Account account = accountService.getAccount(4555L);
 
 		// then
+	 	// accountService.getAccount() 했을 때 accountRepository 가  findById() 를 1번 불렸다
+		verify(accountRepository, times(1)).findById(anyLong());
+		//
+		verify(accountRepository, times(1)).findById(captor.capture());
+		assertEquals(4555L, captor.getValue());
+		assertNotEquals(45515L, captor.getValue());
+		// accountRepository 가  save 하지 않았다
+		verify(accountRepository, times(0)).save(any());
 		assertEquals("65789", account.getAccountNumber());
 		assertEquals(AccountStatus.UNREGISTERED, account.getAccountStatus());
 	}
+
+
+		@Test
+	@DisplayName("계좌 조회 실패 음수로 조회")
+	void testFailedToSearchAccount() {
+		// given
+		// when
+		RuntimeException exception = assertThrows(RuntimeException.class,
+					() -> accountService.getAccount(-10L));
+			// accountService.getAccount(-10L) 동작을 시켰을 때 RuntimeException.class 가 발생할 것이다
+
+		// then
+		assertEquals("Minus", exception.getMessage());
+	}
+
+
 
 	@Test
 	@DisplayName("Test 이름 변경")
